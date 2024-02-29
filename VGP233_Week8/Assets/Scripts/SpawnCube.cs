c84df9528e49eca78545f359c8812b05ce0d3693
NicholasOkovic <139954443+NicholasOkovic@users.noreply.github.com>
2024-02-29 01:43:13 -0800

    played around with raycasting

--- VGP233_Week8/Assets/Scripts/SpawnCube.cs
+++ VGP233_Week8/Assets/Scripts/SpawnCube.cs
@@ -20,26 +20,45 @@ public class SpawnCube : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            RaycastHit hit;
-            Debug.DrawRay(ray.origin, ray.direction * RayLength, Color.red, RayTime);
 
-            if (Physics.Raycast(ray, out hit, RayLength, layerMask))
-            {
-                //hit
-                Debug.Log(hit.point);
-                Instantiate(_cubeToSpawn, hit.point, Quaternion.identity);
-            }
-            else
-            {
-                 Debug.Log("no hit");
-                //not hit
-            }
+        UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject();
+        bool isOverUI = UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject();
 
-           
-        }
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        Debug.DrawRay(ray.origin, ray.direction * RayLength, Color.red, RayTime);
+
+        bool raycast = Physics.Raycast(ray, out hit, RayLength, layerMask);
+
+        Debug.Log(hit.transform.name);
+
+
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //    RaycastHit hit;
+        //    Debug.DrawRay(ray.origin, ray.direction * RayLength, Color.red, RayTime);
+
+
+
+        //    if (Physics.Raycast(ray, out hit, RayLength, layerMask))
+        //    {
+        //        //hit
+        //        Debug.Log(hit.transform.name);
+        //        //Debug.Log(hit.point);
+        //        Instantiate(_cubeToSpawn, hit.point, Quaternion.identity);
+        //    }
+        //    else
+        //    {
+        //         Debug.Log("no hit");
+        //        //not hit
+        //    }
+
+
+        // }
     }
 }
